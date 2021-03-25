54d39a38d43003381e6f624212dc1634e5d7856b
ddr14 <david.dryburgh@sky.uk>
2021-03-25 12:52:38 +0000

    added update route for engineers with modify query

--- src/main/java/com/dryburgh/web/domesticsrepairs/business/engineers/EngineerHandler.java
+++ src/main/java/com/dryburgh/web/domesticsrepairs/business/engineers/EngineerHandler.java
@@ -14,22 +14,14 @@ public class EngineerHandler {
 
 	private EngineerPool engineerPool;
 	private final EngineerRepository engineerRepository;
-	
+
 	@Autowired
 	public EngineerHandler(EngineerRepository engineerRepository) {
 		engineerPool = new EngineerPool();
 		this.engineerRepository = engineerRepository;
 	}
-	
-	public Engineer getAvailableEngineerForAppointment() {
-		return engineerPool.getEngineerFromList();
-	}
 
-	public void createNewEngineer(Engineer newEngineer) {
-		engineerRepository.save(newEngineer);
-	}
-	
-	public List<Engineer> getAllEngineers(){
+	public List<Engineer> getAllEngineers() {
 		Iterable<Engineer> allEngineers = engineerRepository.findAll();
 		List<Engineer> engineersList = new ArrayList<>();
 		allEngineers.forEach(engineer -> {
@@ -42,5 +34,23 @@ public class EngineerHandler {
 			engineersList.add(listEngineer);
 		});
 		return engineersList;
+	}
+
+	public Engineer getEngineerByEngineerId(long engineerId) {
+		return engineerRepository.findById(engineerId).get();
+	}
+
+	public Engineer getAvailableEngineerForAppointment() {
+		return engineerPool.getEngineerFromList();
+	}
+
+	public void createNewEngineer(Engineer newEngineer) {
+		engineerRepository.save(newEngineer);
+	}
+
+	public void updateEngineerDetails(long engineerId, Engineer updateEngineer) {
+		engineerRepository.updateEngineer(engineerId, updateEngineer.getEngineerName(),
+				updateEngineer.getEngineerEmail(), updateEngineer.getEngineerPhoneNumber(),
+				updateEngineer.getEngineerPassword());
 	}
 }
--- src/main/java/com/dryburgh/web/domesticsrepairs/web/EngineerController.java
+++ src/main/java/com/dryburgh/web/domesticsrepairs/web/EngineerController.java
@@ -3,10 +3,14 @@ package com.dryburgh.web.domesticsrepairs.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dryburgh.web.domesticsrepairs.business.engineers.EngineerHandler;
@@ -24,13 +28,28 @@ public class EngineerController {
 	}
 	
 	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
 	public List<Engineer> getEngineers() {
 		return engineerHandler.getAllEngineers();
 	}
 	
+	@GetMapping("/{engineerId}")
+	@ResponseStatus(HttpStatus.OK)
+	public Engineer getEngineerByEngineerId(@PathVariable(name="engineerId") long engineerId) {
+		return engineerHandler.getEngineerByEngineerId(engineerId);
+	}
+	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public String createNewEngineer(@RequestBody Engineer newEngineer) {
 		engineerHandler.createNewEngineer(newEngineer);
 		return "New Engineer Created";
+	}
+	
+	@PutMapping("/{engineerId}")
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	public String updateEngineer(@PathVariable(name="engineerId") long engineerId, @RequestBody Engineer updateEngineer) {
+		engineerHandler.updateEngineerDetails(engineerId, updateEngineer);
+		return "Updated Engineer with ID: " + engineerId;
 	}
 }
